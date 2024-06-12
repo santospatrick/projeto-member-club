c8da83b04d8ca1510de1217e4de7c1cd5bd62ddc
Patrick Santos <patrickspteixeira@gmail.com>
2024-06-11 23:56:34 -0300

    adiciona utilitarios de tipografia, fundo colorido do perfil e estilos para cards

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@
 import './styles/global.css';
 import './styles/header.css';
 import './styles/form.css';
+import './styles/card.css';
+import './styles/content.css';
+import './styles/typography.css';
 
 import './modules/page-load.js';
