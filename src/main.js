66a06be986deedc41d1827ff451545bdba1d02ef
Patrick Santos <patrickspteixeira@gmail.com>
2024-06-12 00:11:27 -0300

    adiciona utilitarios para cores, uppercase para fonts corretas e cor verde com opacidade

--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ import './styles/form.css';
 import './styles/card.css';
 import './styles/content.css';
 import './styles/typography.css';
+import './styles/colors.css';
 
 import './modules/page-load.js';
