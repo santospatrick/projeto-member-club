0993e29c1203cee75a19505663a69d7f20df478d
Patrick Santos <patrickspteixeira@gmail.com>
2024-06-11 21:45:14 -0300

    adiciona webpack e estrutura basica html js

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    target: 'web',
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src', 'main.js'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        liveReload: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+        })
+    ],
+}
